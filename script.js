867194f018cbdef6738bcf8a4875dca3beee7a36
Vinicius de Alencar Freire <alencarfreire@gmail.com>
2024-07-01 00:03:17 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+let outputText = document.getElementById("outputText");
+let afterResult = document.querySelector(".after-result");
+let buttonCopy = document.querySelector(".button-copy");
+let boxResult = document.querySelector(".box-result");
+
+function adjustStyle() {
+  afterResult.style.display = "none";
+  buttonCopy.style.display = "block";
+  boxResult.style.justifyContent = "space-between";
+  outputText.style.height = "80%";
+}
+
+function encryptText() {
+  let text = document.getElementById("inputText").value;
+  let encryptedText = text
+    .replace(/e/g, "enter")
+    .replace(/i/g, "imes")
+    .replace(/a/g, "ai")
+    .replace(/o/g, "ober")
+    .replace(/u/g, "ufat");
+  outputText.innerText = encryptedText;
+  adjustStyle();
+}
+
+function decryptText() {
+  let text = document.getElementById("inputText").value;
+  let encryptedText = text
+    .replace(/enter/g, "e")
+    .replace(/imes/g, "i")
+    .replace(/ai/g, "a")
+    .replace(/ober/g, "o")
+    .replace(/ufat/g, "u");
+  outputText.innerText = encryptedText;
+  adjustStyle();
+}
+
+function copyToClipboard() {
+  let text = document.getElementById("outputText");
+  text.select();
+  text.setSelectionRange(0, 99999); // Para dispositivos móveis
+  navigator.clipboard.writeText(text.value);
+}
